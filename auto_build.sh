cf1f2576e23140dceefc7c334d07eaa771192d96
root <root@foremanprodpoc.testlab.local>
2017-05-31 15:58:51 -0400

    My dev server

--- auto_build.sh
+++ auto_build.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-hammer -u admin -p redhat  host create --name $1  --compute-resource-id=1 --hostgroup-id=2
-hammer -u admin -p redhat  host start --name $1.testlab.local
-
+sh /root/build.sh test001
